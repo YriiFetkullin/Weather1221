4bd1f366a908548a26144378c746cd4fcd724dc8
Fetkullin Yrii <02011994jkl@mail.ru>
2025-05-25 12:17:02 +0300

    Initial Commit

---
+++ TestTask1221/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestTask1221
+//
+//  Created by Юрий Феткуллин on 25.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestTask1221/TestTask1221App.swift
@@ -0,0 +1,17 @@
+//
+//  TestTask1221App.swift
+//  TestTask1221
+//
+//  Created by Юрий Феткуллин on 25.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTask1221App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
